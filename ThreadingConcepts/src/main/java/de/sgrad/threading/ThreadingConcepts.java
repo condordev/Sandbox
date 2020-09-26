1f6fa51636990984d046c3f9923be47802a59469
condordev <sgrad07@gmail.com>
2020-09-26 18:55:29 +0200

    added MyCallable

--- ThreadingConcepts/src/main/java/de/sgrad/threading/ThreadingConcepts.java
+++ ThreadingConcepts/src/main/java/de/sgrad/threading/ThreadingConcepts.java
@@ -1,7 +1,12 @@
 package de.sgrad.threading;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class ThreadingConcepts {
-    public static void main(String[] args){
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Thread t1 = new Thread(()-> {
             for (int i = 0; i < 100; i++){
                 try{
@@ -13,5 +18,23 @@ public class ThreadingConcepts {
             }
         });
         t1.start();
+
+        Future<Integer> result = Executors.newFixedThreadPool(1).submit(new MyCallable());
+        System.out.println(result.get());
+
     }
+
+
+    static class MyCallable implements Callable<Integer>{
+        @Override
+        public Integer call() throws Exception {
+            int result = 0;
+            System.out.println("Name: " + Thread.currentThread().getName());
+            for (int i = 0; i < 1000 ; ++i){
+                result++;
+            }
+            return result;
+        }
+    }
+
 }
